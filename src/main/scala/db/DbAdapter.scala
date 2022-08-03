7d98b3c179dc6fac52282481d3f56172dc013301
Ryan Pierre <info@ryanpierre.com>
2022-08-03 22:47:16 +0100

    get rough version of db adapter api working

--- src/main/scala/db/DbAdapter.scala
+++ src/main/scala/db/DbAdapter.scala
@@ -2,9 +2,9 @@ package main.db
 
 import scala.collection.mutable.ArrayBuffer
 import main.model.Item
+import scala.collection.mutable.LinkedHashMap
 
 class DbAdapter {
-  // TODO: This could be refactored so we could test without actually reading a real json file
   def read(table: String): ArrayBuffer[ujson.Obj] = {
     val filename = table + ".json"
     val jsonString = os.read(os.pwd/"src"/"main"/"resources"/filename)
@@ -12,17 +12,35 @@ class DbAdapter {
     data.value.asInstanceOf[ArrayBuffer[ujson.Obj]]
   }
 
-  // TODO: Writes the new data to the database
-  def write[T](table: String, data: ArrayBuffer[T]) = {
-
+  def readRaw(table: String): ujson.Arr = {
+    val filename = table + ".json"
+    val jsonString = os.read(os.pwd/"src"/"main"/"resources"/filename)
+    ujson.read(jsonString).asInstanceOf[ujson.Arr]
   }
 
-  // TODO: Cleans the database back to the base state
-  def flush: Boolean = {
-    true
+  def write(table: String, data: ArrayBuffer[Item]) = {
+    val filename = table + ".json"
+    val jsonString = os.read(os.pwd/"src"/"main"/"resources"/filename)
+
+    val newData = ujson.Arr(data.map(item => {
+      val id = ujson.Num(item.id.toDouble)
+      val name =  ujson.Str(item.name)
+      val price =  ujson.Num(item.price.toDouble)
+      val quantity = ujson.Num(item.quantity.toDouble)
+      val availableLocales = ujson.Value(ujson.Arr(item.availableLocales))
+
+      ujson.Obj(LinkedHashMap("id" -> id, "name" -> name, "price" -> price, "quantity" -> quantity, "availableLocales" -> availableLocales))
+    }))
+
+    os.remove(os.pwd/"src"/"main"/"resources"/"items.json")
+    os.write(os.pwd/"src"/"main"/"resources"/filename, newData)
   }
 
-  // TODO: This could be refactored to be passed the db as a param, and then mocked better in the test
+  def reset: Unit = {
+    os.remove(os.pwd/"src"/"main"/"resources"/"items.json")
+    os.write(os.pwd/"src"/"main"/"resources"/"items.json", os.read(os.pwd/"src"/"main"/"resources"/"items.base.json"))
+  }
+
   def getItems(): ArrayBuffer[Item] = {
     val db = this.read("items")
 
@@ -31,18 +49,34 @@ class DbAdapter {
       val name = item("name").value.asInstanceOf[String]
       val price = item("price").value.asInstanceOf[Double]
       val quantity = item("quantity").value.asInstanceOf[Double].toInt
-      val locales = item("availableLocales").arr.map(_.value.asInstanceOf[String]).toList
-      new Item(id, name, price, quantity, locales)
+      val availableLocales = item("availableLocales").arr.map(_.value.asInstanceOf[String]).toList
+      new Item(id, name, price, quantity, availableLocales)
     })
   }
 
-  // TODO: Replaces an item in the database with the matching id of the one we provided
-  def updateItem(id: Int, newItem: Item): Boolean = {
-    true
+  def updateItem(id: Int, newItem: Item): Unit = {
+    val items = this.getItems()
+    val newData = items.map(i => i.id match {
+      case x if x == id => newItem
+      case _ => i
+    })
+
+    this.write("items", newData)
   }
 
-  // TODO: Creates a new item in the database
-  def createItem(newItem: Item): Boolean = {
-    true
+  def createItem(newItem: Item): Unit = {
+      val id = ujson.Num(newItem.id.toDouble)
+      val name =  ujson.Str(newItem.name)
+      val price =  ujson.Num(newItem.price.toDouble)
+      val quantity = ujson.Num(newItem.quantity.toDouble)
+      val availableLocales = new ujson.Arr(ArrayBuffer(newItem.availableLocales))
+      val newJsonObj = LinkedHashMap("id" -> id, "name" -> name, "price" -> price, "quantity" -> quantity, "availableLocales" -> availableLocales)
+      val newJson = ujson.Value(newJsonObj)
+      
+      val db = this.readRaw("items")
+      db.arr.append(newJson)
+
+      os.remove(os.pwd/"src"/"main"/"resources"/"items.json")
+      os.write(os.pwd/"src"/"main"/"resources"/"items.json", db)
   }
 }
